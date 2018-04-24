bc405d0eb9e3c027c86c93498364f19a30adf6ba
VictoriaMaria1707 <38673421+VictoriaMaria1707@users.noreply.github.com>
2018-04-23 23:00:53 -0500

    sd

---
+++ MVCCORE/Models/Medicamento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCORE.Models
+{
+    public class Medicamento
+    {
+        public  int MedicamentoId { get; set; }
+        //campo requerido con mensaje personalizado
+        [Required(ErrorMessage ="El nombre del medicamento es requerido")]
+        //mostrar
+        [Display(Name ="Nombre de Medicamento")]
+        //definir el largo de caracteres max y minimo y mensaje de error
+        [StringLength(220,MinimumLength =3,ErrorMessage ="El nombre del medicamento tiene que tener minimo 3 caracteries y maximo 220")]
+        public string NombreMedicamiento { get; set; }
+
+        [Required(ErrorMessage = "la fecha de expiracion es requerido")]
+        [Display(Name = "Fecha de expiracion")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_Expira { get; set; }
+
+        [Required(ErrorMessage = "la fecha de fabricacion es requerido")]
+        [Display(Name = "Fecha de fabricacion")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_fabricado { get; set; }
+
+        [Required(ErrorMessage = "la fecha de ingreso es requerido")]
+        [Display(Name = "Fecha de ingreso")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_Ingreso { get; set; }
+
+    }
+}
---
+++ MVCCORE/Models/TipoMedicamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCORE.Models
+{
+    public class TipoMedicamento
+    {
+        public int TipoMedicamentoId { get; set; }
+
+        public string Detalle { get; set; }
+    }
+
+    
+}
